56a1b7776aebf901e90fa172f64b545ac56a6299
rlawogns <rlawogns0411@naver.com>
2022-08-08 14:51:38 +0900

    feat : add Meeting model

---
+++ frontend/app/lib/model/meeting.dart
@@ -0,0 +1,11 @@
+class Meeting {
+  int? meetingId;
+  int? roomId;
+  String? date;
+  String? startTime;
+  String? endTime;
+  String? title;
+  String? content;
+  Meeting(this.meetingId, this.roomId, this.date, this.startTime, this.endTime,
+      this.title, this.content);
+}
