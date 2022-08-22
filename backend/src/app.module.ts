6ff88bb72fa8f9078824941be11eb11bcb8c4a98
rlawogns <rlawogns0411@naver.com>
2022-08-22 18:36:02 +0900

    feat : Add Participants Api

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,10 +5,13 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { GroupModule } from './group/group.module';
 import { RoomModule } from './room/room.module';
+import { MeetingModule } from './meeting/meeting.module';
+import { ParticipantService } from './participant/participant.service';
+import { ParticipantModule } from './participant/participant.module';
 const settings = require('../ormconfig.json');
 
 @Module({
-  imports: [TypeOrmModule.forRoot(settings),UserModule, GroupModule, RoomModule],
+  imports: [TypeOrmModule.forRoot(settings),UserModule, GroupModule, RoomModule, MeetingModule, ParticipantModule],
   controllers: [AppController],
   providers: [AppService],
 })
