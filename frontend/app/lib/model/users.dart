95415602f3792c724e668bdb4bfeca862855ee89
rlawogns <rlawogns0411@naver.com>
2022-08-25 15:29:20 +0900

    feat : Add urldata

--- frontend/app/lib/model/users.dart
+++ frontend/app/lib/model/users.dart
@@ -1,12 +1,14 @@
+import 'package:app/urldata.dart';
 import 'package:dio/dio.dart';
 
 class Users {
   var dio = Dio();
+  UrlData urlData = UrlData();
   Future<List<String>> getGroupUserId(body) async {
     List<String> users = [];
     print(body);
     await dio
-        .get('http://192.168.0.30:3000/users/group', queryParameters: body)
+        .get('${urlData.url}/users/group', queryParameters: body)
         .then((value) {
       for (var i in value.data["data"]) {
         users.add(i["slackId"]);
@@ -19,7 +21,7 @@ class Users {
     print(body);
     String slackId = '';
     await dio
-        .get('http://192.168.0.30:3000/users/name', queryParameters: body)
+        .get('${urlData.url}/users/name', queryParameters: body)
         .then((value) {
       print(value.data["data"]);
       print(value.data["data"]["slackId"]);
@@ -31,7 +33,7 @@ class Users {
   Future<String> getUserIdName(body) async {
     String name = '';
     await dio
-        .get('http://192.168.0.30:3000/users/slack', queryParameters: body)
+        .get('${urlData.url}/users/slack', queryParameters: body)
         .then((value) {
       if (value.data["data"] != null) {
         name = value.data["data"]["name"];
@@ -44,7 +46,7 @@ class Users {
   Future<String> getEmailUserId(body) async {
     String slackId = '';
     await dio
-        .get('http://192.168.0.30:3000/users/email', queryParameters: body)
+        .get('${urlData.url}/users/email', queryParameters: body)
         .then((value) {
       if (value.data["data"] != null) {
         slackId = value.data["data"]["name"];
@@ -54,6 +56,6 @@ class Users {
   }
 
   Future<void> createUser(body) async {
-    await dio.post('http://192.168.0.30:3000/users/create', data: body);
+    await dio.post('${urlData.url}/users/create', data: body);
   }
 }
---
+++ frontend/app/lib/urldata.dart
@@ -0,0 +1,3 @@
+class UrlData {
+  String url = 'http://192.168.0.30:3001';
+}
